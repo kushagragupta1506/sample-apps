17c53401b3da9d78dc88acf09d3e351804d1322b
Mehmet Sahin AYCICEK <sahinaycicek93@gmail.com>
2023-08-04 10:42:45 +0300

    updated

--- v2-android-app/ShoppingApp/app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ v2-android-app/ShoppingApp/app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.shoppingapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.appsamurai.storyly.StoryGroupSize
-import com.appsamurai.storyly.StorylyInit
-import com.appsamurai.storyly.StorylyView
+import android.util.Log
+import com.appsamurai.storyly.*
+import com.appsamurai.storyly.analytics.StorylyEvent
 import com.appsamurai.storyly.config.StorylyConfig
 import com.appsamurai.storyly.config.styling.group.StorylyStoryGroupStyling
 import com.example.shoppingapp.databinding.ActivityMainBinding
@@ -24,18 +26,154 @@ class MainActivity : AppCompatActivity() {
         val storylyView = findViewById<StorylyView>(R.id.storyly_view)
 
         storylyView.storylyInit = StorylyInit(
-            storylyId ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjcxMzcsImFwcF9pZCI6MTE3NDYsImluc19pZCI6MTI1ODJ9.k7IVUbx4b23WTobh7u-ZIAYMdjN1xIDyA8z5WWncWbU",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOjcxMzcsImFwcF9pZCI6MTMxMTUsImluc19pZCI6MTc5ODZ9.jf8hLmrRQ7mhDDlP2psDMutfFXM0Ib3eqaeDWEIsbg0",
             StorylyConfig.Builder()
                 .setStoryGroupStyling(
                     StorylyStoryGroupStyling.Builder()
-                        .setIconHeight(240)
-                        .setIconWidth(240)
+                        .setIconHeight(350)
+                        .setIconWidth(275)
                         .setIconCornerRadius(30)
                         .setSize(StoryGroupSize.Custom)
                         .build()
                 )
                 .build()
         )
+
+        storylyView.storylyListener = object : StorylyListener {
+            var initialLoad = true
+
+            override fun storylyActionClicked(storylyView: StorylyView, story: Story) {
+
+                story.media.actionUrl.let { url ->
+                    Log.d("-----------[storyly]--------", "IntegrationActivity:storylyActionClicked - forwarding to url {$url} and ${story.media.type} ")
+                    startActivity(
+                        Intent(Intent.ACTION_VIEW).apply {
+                            data = Uri.parse("$url")
+                        }
+                    )
+                }
+
+                storylyView.dismiss(animationResId = 1)
+
+
+                /*val goSecondPage = Intent(applicationContext, MainActivity2::class.java)
+                startActivity(goSecondPage) */ // go to other page
+
+                /* val fragment = DemoFragment()
+                 fragment.onCloseClick = {
+                     removeFragments()
+                 }
+                 showExternalFragment(fragment) */
+            }
+
+            override fun storylyLoaded(
+                storylyView: StorylyView,
+                storyGroupList: List<StoryGroup>,
+                dataSource: StorylyDataSource
+            ) {
+                //Log.d("[storyly]", "IntegrationActivity:storylyLoaded - storyGroupList size {${storyGroupList.size}} - source {$dataSource}")
+
+                /*if (initialLoad && storyGroupList.isNotEmpty()) {
+                    initialLoad = false
+                    storylyView.visibility = View.VISIBLE
+                    Log.d("customParameter", "-----> {${customData}}" )
+                }*/
+
+                Log.d("storylyURL", "URL:${StorylyDataSource.API}")
+
+                if (dataSource == StorylyDataSource.Local) {
+                    Log.d("[Local]", "IntegrationActivity:storylyLoaded - storyGroupList size {${storyGroupList.size}} - source {$dataSource}")
+                }
+
+                /*if (dataSource == StorylyDataSource.API) {
+                    storylyView.openStory("62376", "542324")
+                }*/
+            }
+
+            override fun storylyLoadFailed(
+                storylyView: StorylyView,
+                errorMessage: String
+            ) {
+                Log.d("[storyly]", "IntegrationActivity:storylyLoadFailed - error {$errorMessage}")
+
+            }
+
+            override fun storylyUserInteracted(
+                storylyView: StorylyView,
+                storyGroup: StoryGroup,
+                story: Story,
+                storyComponent: StoryComponent
+            ) {
+
+                val quizComponent = storyComponent as? StoryQuizComponent
+                val pollComponent = storyComponent as? StoryPollComponent
+                val emojiComponent = storyComponent as? StoryEmojiComponent
+                val ratingComponent = storyComponent as? StoryRatingComponent
+                val promoCodeComponent = storyComponent as? StoryPromoCodeComponent
+                val commentComponent = storyComponent as? StoryCommentComponent
+
+                Log.d("--------------storyly-------------------", "StoryComponent{${storyComponent}} and {${story}}")
+                Log.d("--------------StoryComponent-------------------", "promoCodeComponent:${promoCodeComponent?.text}")
+                Log.d("emojiComponent","customPayload:${emojiComponent?.customPayload}")
+                Log.d("quizComponent","customPayload:${quizComponent?.customPayload} ")
+
+
+
+            }
+
+            override fun storylyEvent(storylyView: StorylyView, event: StorylyEvent,
+                                      storyGroup: StoryGroup?, story: Story?,
+                                      storyComponent: StoryComponent?) {
+                val quizComponent = storyComponent as? StoryQuizComponent
+                val pollComponent = storyComponent as? StoryPollComponent
+                val emojiComponent = storyComponent as? StoryEmojiComponent
+                val ratingComponent = storyComponent as? StoryRatingComponent
+                val promoCodeComponent = storyComponent as? StoryPromoCodeComponent
+                val commentComponent = storyComponent as? StoryCommentComponent
+
+                /*val customPay = when (storyComponent?.type){
+                    Emoji -> storyComponent as? StoryEmojiComponent
+                    Quiz -> storyComponent as? StoryQuizComponent
+                    Poll -> storyComponent as? StoryPollComponent
+                    Rating -> storyComponent as? StoryRatingComponent
+                    PromoCode -> storyComponent as? StoryPromoCodeComponent
+                    //SwipeAction -> storyComponent as? Story
+                    //ButtonAction -> storyComponent as?
+                    //Text -> storyComponent as? StoryEmojiComponent
+                    //Image -> storyComponent as? StoryEmojiComponent
+                    //Countdown -> storyComponent as? StoryComponent
+                    //ProductTag -> storyComponent as? StoryEmojiComponent
+                    Comment -> storyComponent as? StoryCommentComponent
+                    //Video -> storyComponent as? StoryEmojiComponent
+                    //Vod -> storyComponent as? StoryEmojiComponent
+                    //null -> storyComponent as? StoryEmojiComponent
+                    SwipeAction -> TODO()
+                    ButtonAction -> TODO()
+                    Text -> TODO()
+                    Image -> TODO()
+                    Countdown -> TODO()
+                    ProductTag -> TODO()
+                    Video -> TODO()
+                    Vod -> TODO()
+                    null -> TODO()
+                }*/
+
+                //Log.d("Storyly","customPayload:${emojiComponent?.customPayload} ")
+                Log.d("event----->","StoryPaused:${StorylyEvent.StoryPaused} ") // You can track StoryViewed event.
+                Log.d("event----->","StoryViewed:${StorylyEvent.StoryViewed} ") // You can track StoryViewed event.
+                Log.d ("Event", "PromoCode:${StorylyEvent.StoryPromoCodeCopied}")
+
+
+
+                Log.d("--------------StoryType-------------------", "StoryComponent{${storyComponent?.type}}")
+
+
+                Log.d("--------------storyly-------------------","story:${story?.seen} and ${story?.uniqueId}")
+                Log.d("--------------Index-------------------","storyGroupIndex: ${storyGroup?.index} and storyIndex: ${story?.index}")
+
+            }
+
+        }
 
     }
 }
--- v2-ios-app/myStoryly/myStoryly/ViewController.swift
+++ v2-ios-app/myStoryly/myStoryly/ViewController.swift
@@ -10,9 +10,17 @@ import Storyly
 
 class ViewController: UIViewController {
     
-    let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjcxMzcsImFwcF9pZCI6MTE3NDYsImluc19pZCI6MTI1ODJ9.k7IVUbx4b23WTobh7u-ZIAYMdjN1xIDyA8z5WWncWbU"
+    let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjM1NzQsImFwcF9pZCI6Njk3NSwiaW5zX2lkIjo3MjIzfQ.N3lyKBjtEYroXSnSe5Wl93CpLMv1G_pmRa-k1K-CgHM"
     
     let userPropertiesData = [
+        "first_name" : "John",
+        "image_product_1" : "https://contents.mediadecathlon.com/p2058435/ec1165ccbcdc2abc0ef41b98f8c53e77/p2058435.jpg?format=auto&quality=70&f=650x0",
+        "image_product_2" : "https://contents.mediadecathlon.com/p2431723/bea97805143565ce7ab8e0e3680ce3e5/p2431723.jpg?format=auto&quality=70&f=650x0",
+        "image_product_3" : "https://contents.mediadecathlon.com/p2427007/6547648c60fc3c1d643fe2968cec84a7/p2427007.jpg?format=auto&quality=70&f=650x0",
+        "name_product_1" : "Umbrella High Resistance - Camouflage Grey",
+        "name_product_2" : "Rain Cover for Hiking Backpack - 10/20 L",
+        "name_product_3" : "Waterproof Mobile Phone Pouch Large IPX8",
+        "number_of_items_left_in_cart": "2",
         "username" : "John",
         "gift_amount" : "35%",
         "cover_img" : "https://cdn.pixabay.com/photo/2022/10/15/21/23/cat-7523894_1280.jpg"
@@ -34,6 +42,7 @@ class ViewController: UIViewController {
                                    .setIconCornerRadius(radius: 12)
                                    .build()
                             )
+                           .setTestMode(isTest: true)
                             .build()
         )
         
@@ -41,7 +50,7 @@ class ViewController: UIViewController {
         // the class(indicated with self) extends StorylyDelegate
         self.storylyView.delegate = self // Override event functions
         // Do any additional setup after loading the view.
-        //self.storylyView.storylyInit.config.userData = userPropertiesData
+        self.storylyView.storylyInit.config.userData = userPropertiesData
         
         
     }
